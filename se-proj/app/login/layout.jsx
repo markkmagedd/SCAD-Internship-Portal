a58384fbf1a5aada74184e6fb5e1e97399763642
xxMosho <markmagedamazon@gmail.com>
2025-04-27 15:52:23 +0300

    login page

---
+++ se-proj/app/login/layout.jsx
@@ -0,0 +1,8 @@
+export default function LoginLayout({ children }) {
+  return (
+    <div className="grid grid-cols-2 min-h-screen bg-[#273F4F]">
+      <div className="flex items-center justify-center">{children}</div>
+      <div className="flex items-center justify-center"></div>
+    </div>
+  );
+}
---
+++ se-proj/app/login/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginCard from "@/components/loginCard";
+import { Separator } from "@radix-ui/react-separator";
+
+function page() {
+  return (
+    <div>
+      <LoginCard></LoginCard>
+    </div>
+  );
+}
+
+export default page;
